a4b026c4e11dea96eeac77b3bf007cb1d9e54eda
AlimCetin <Alim.cetin37@gmail.com>
2023-12-15 23:57:25 +0300

    START :)

---
+++ Backend/cafe/src/main/java/com/alicetin/cafe/utils/ERoles.java
@@ -0,0 +1,5 @@
+package com.hamitmizrak.tech_3_springreact.utils;
+
+public enum ERoles {
+    ADMIN,CUSTOMER,COMPANY
+}
---
+++ Backend/cafe/src/main/java/com/alicetin/cafe/utils/PersistLink.java
@@ -0,0 +1,9 @@
+package com.hamitmizrak.tech_3_springreact.utils;
+
+public class PersistLink {
+
+//    Backend Project Url: http://localhost:8080/
+//    Database Url       : http://localhost:8080/h2-console
+//    Swagger Ui         : http://localhost:8080/swagger-ui/index.html
+
+}
